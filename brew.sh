a9c0bb4833bf6de523e3e566b096e47f3d33952b
Jack Lawson <ajacksified@gmail.com>
2023-11-11 13:58:59 -0500

    Latest updates from new build

---
+++ brew.sh
@@ -0,0 +1,2 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install vim tmux gpg git nvm curl ag nvim autojump bash reattach-to-user-namespace
